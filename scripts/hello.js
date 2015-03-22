9a1fcd639dc7501fe266193437206cf61e927486
Lucas F <lfayolle1@gmail.com>
2015-03-22 18:55:16 +0100

    light landing anim homepage

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,8 +1,7 @@
-
-/* jshint white:false */
-
-(function() {
-  'use strict';
-  console.log('\'Allo from CoffeeScript!');
-
-}).call(this);
+$(document).ready(function() {
+	setTimeout(function(){
+		TweenMax.to('.landing header', 1, {opacity:1,ease:Power4.easeOut});
+		TweenMax.to('.headline', 0.7, {opacity:1,top:0,ease:Power4.easeOut,delay:0.5});
+		TweenMax.to('.searchbar', 0.7, {opacity:1,marginTop:'+=20',ease:Power4.easeOut,delay:0.65});
+	},250);
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,4 @@
-console.log('\'Allo \'Allo!');
+TweenMax.set('.headline', {opacity:0,top:30});
+TweenMax.set('.searchbar', {opacity:0,marginTop:'-=20'});
+TweenMax.set('.landing header', {opacity:0});
+        
