9cd96134e88b4115ea4bb4886c6cddff23c1f793
Lucas F <lfayolle1@gmail.com>
2015-03-22 21:13:41 +0100

    some anime + tab fade

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-	setTimeout(function(){
-		TweenMax.to('.landing header', 1, {opacity:1,ease:Power4.easeOut});
-		TweenMax.to('.headline', 0.7, {opacity:1,top:0,ease:Power4.easeOut,delay:0.5});
-		TweenMax.to('.searchbar', 0.7, {opacity:1,marginTop:'+=20',ease:Power4.easeOut,delay:0.65});
-	},250);
+	TweenMax.to('.landing header', 1, {opacity:1,ease:Power4.easeOut});
+	TweenMax.to('.headline', 0.7, {opacity:1,top:0,ease:Power4.easeOut,delay:0.5});
+	TweenMax.to('.searchbar', 0.7, {opacity:1,marginTop:'+=20',ease:Power4.easeOut,delay:0.65});
+
+	TweenMax.to('#content.salon .main-content', 0.8, {top:0,opacity:1,ease:Power2.easeOut});
+	// TweenMax.to('#content.salon .sidebar', 0.8, {marginTop:-90,opacity:1,ease:Power4.easeOut});
 });
